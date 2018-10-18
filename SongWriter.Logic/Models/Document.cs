8fbfdfd98698b4a886c6beb265dfc8d24c7faad9
akang8 <kang08@hotmail.com>
2018-10-17 21:53:44 -0400

    Logic model

---
+++ SongWriter.Logic/Models/Document.cs
@@ -0,0 +1,19 @@
+﻿using SongWriter.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SongWriter.Logic.Models
+{
+    /// <summary>
+    /// Represents a document
+    /// </summary>
+    public class Document : INamedIdentifiable, IText
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Text { get; set; }
+    }
+}
