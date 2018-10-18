8fb271ab6dbc8abfbdffd95de887b8639e5c5cd2
akang8 <kang08@hotmail.com>
2018-10-17 21:53:31 -0400

    Db context, with first model

--- SongWriter.Data/AppDbContext.cs
+++ SongWriter.Data/AppDbContext.cs
@@ -8,6 +8,9 @@ namespace SongWriter.Data
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<Document> Document { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Document> Documents { get; set; }
     }
 }
--- SongWriter.Data/Models/Document.cs
+++ SongWriter.Data/Models/Document.cs
@@ -1,16 +1,22 @@
 ﻿using SongWriter.Core;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace SongWriter.Data.Models
 {
+    /// <summary>
+    /// Represents the data storage of a document
+    /// </summary>
     public class Document : INamedIdentifiable, IText
     {
         public int Id { get; set; }
 
+        [MaxLength(255)]
         public string Name { get; set; }
 
+        [MaxLength]
         public string Text { get; set; }
     }
 }
