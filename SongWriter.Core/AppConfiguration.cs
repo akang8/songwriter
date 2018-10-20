68d4bf9bd0aee998e13780b4788eee2af72693ac
akang8 <kang08@hotmail.com>
2018-10-20 14:26:26 -0400

    Add comment for core object

--- SongWriter.Core/AppConfiguration.cs
+++ SongWriter.Core/AppConfiguration.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace SongWriter.Core
 {
+    /// <summary>
+    /// Object representing the appsettings.json
+    /// </summary>
     public class AppConfiguration
     {
         public bool ResetData { get; set; }
