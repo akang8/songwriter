bc55f99c6653c483be6bac16b8eaa44b7f47a1ff
akang8 <kang08@hotmail.com>
2018-10-22 15:09:19 -0400

    Some initial code to get all documents from server

--- SongWriter.Web/ClientApp/components/home-page.vue
+++ SongWriter.Web/ClientApp/components/home-page.vue
@@ -2,17 +2,35 @@
     <div>
         <h1>Song Writer</h1>
         <a href="#" @click.prevent="createSong" class="btn btn-primary"><icon :icon="['fas', 'plus-square']" /> Add Document</a>
+        {{documents}}
     </div>
 </template>
 <script>
     export default {
         data() {
-            return {}
+            return {
+                documents: []
+            }
         },
         methods: {
             createSong() {
                 this.$router.push({ name: 'DocumentCreate' });
+            },
+            async loadDocuments() {
+                try {
+                    var result = await this.$http.get('/api/document')
+                    if (result) {
+                        this.documents = result.data;
+                    }
+                } catch (err) {
+                    window.alert(err)
+                    console.log(err)
+                }
             }
+        },
+        async created() {
+            // Fire and forget, no need for await here?
+            this.loadDocuments();
         }
     }
 </script>
