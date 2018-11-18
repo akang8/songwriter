97f5b6efc0ff5c7c8e2609580d82620811ac9124
akang8 <kang08@hotmail.com>
2018-11-17 22:24:18 -0500

    Make codeMirroConfig like toastConfig

--- SongWriter.Web/ClientApp/infrastructure/codeMirrorConfig.js
+++ SongWriter.Web/ClientApp/infrastructure/codeMirrorConfig.js
@@ -1,79 +1,78 @@
 import CodeMirror from 'codemirror';
 
-const configure = function () {
-    CodeMirror.defineMode('songwriter', () => {
-        return {
-            token(stream, state) {
-                var ch = stream.peek();
+CodeMirror.defineMode('songwriter', () => {
+    return {
+        token(stream, state) {
+            var ch = stream.peek();
 
 
-                // Handle Comments
-                if (stream.sol()) {
-                    if (stream.match("[")) {
-                        while ((ch = stream.next()) != null)
-                            if (ch == "]") {
-                                stream.eat("]");
-                                return "section";
-                            }
-                    }
-                    else {
-                        if (ch === "!") {
-                            stream.next();
-                            if (stream.eol()) {
-                                return 'start-annotation';
-                            }
-                            else {
-                                return 'marker-annotation';
-                            }
+            // Handle Comments
+            if (stream.sol()) {
+                if (stream.match("[")) {
+                    while ((ch = stream.next()) != null)
+                        if (ch == "]") {
+                            stream.eat("]");
+                            return "section";
                         }
-                        else if (ch === "@") {
-                            stream.next();
-                            if (stream.eol()) {
-                                return 'start-chord';
-                            }
-                            else {
-                                return 'marker-chord';
-                            }
-                        }
-                        else if (ch === "#") {
-                            stream.next();
-                            if (stream.eol()) {
-                                return 'start-lyric';
-                            }
-                            else {
-                                return 'marker-lyric';
-                            }
-                        }
-                    }
-
-                    stream.next()
-                    return null
                 }
                 else {
-                    // Go back one char, should be safe since we know we're not at the SOL
-                    stream.backUp();
-                    var ch = stream.peek();
-
                     if (ch === "!") {
-                        stream.skipToEnd();
-                        return 'annotation';
+                        stream.next();
+                        if (stream.eol()) {
+                            return 'start-annotation';
+                        }
+                        else {
+                            return 'marker-annotation';
+                        }
                     }
                     else if (ch === "@") {
-                        stream.skipToEnd();
-                        return 'chord';
+                        stream.next();
+                        if (stream.eol()) {
+                            return 'start-chord';
+                        }
+                        else {
+                            return 'marker-chord';
+                        }
                     }
                     else if (ch === "#") {
-                        stream.skipToEnd();
-                        return 'lyric';
+                        stream.next();
+                        if (stream.eol()) {
+                            return 'start-lyric';
+                        }
+                        else {
+                            return 'marker-lyric';
+                        }
                     }
-
-                    stream.skipToEnd()
-                    return null
                 }
+
+                stream.next()
+                return null
+            }
+            else {
+                // Go back one char, should be safe since we know we're not at the SOL
+                stream.backUp();
+                var ch = stream.peek();
+
+                if (ch === "!") {
+                    stream.skipToEnd();
+                    return 'annotation';
+                }
+                else if (ch === "@") {
+                    stream.skipToEnd();
+                    return 'chord';
+                }
+                else if (ch === "#") {
+                    stream.skipToEnd();
+                    return 'lyric';
+                }
+
+                stream.skipToEnd()
+                return null
             }
         }
-    });
-}
+    }
+});
 
 
-export default configure;
+
+
